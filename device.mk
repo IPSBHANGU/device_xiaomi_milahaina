11642001abe7654f762cc90303cf34dccb827468
Alexander Koskovich <zvnexus@outlook.com>
2021-08-01 19:40:24 -0700

    milahaina: Pick up USB HAL product makefile.
    Change-Id: Ieb44da5e993590bd1b13dfcee0b2f77c1cb85d79

--- device.mk
+++ device.mk
@@ -143,5 +143,8 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# USB
+$(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/milahaina/milahaina-vendor.mk)
