59e997223eaeb702fe02509d303a72967aef968e
Alexander Koskovich <akoskovich@pm.me>
2021-09-04 01:17:59 -0700

    milahaina: Only advertise eSE capabilities on haydnpro SKUs.
    Change-Id: I436097ccdc344a156e0266f88ced693d05fdb77c

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ $(call inherit-product, vendor/nxp/opensource/sn100x/halimpl/nfc_vendor_product.
 TARGET_USES_NQ_NFC := true
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_haydnpro/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
 PRODUCT_PACKAGES += \
