81aeaac5676ab7fbd6ab2ea21701f101e01b7166
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 15:18:35 +0300

    milahaina: vibrator: add support for aw8697_haptic input device

--- device.mk
+++ device.mk
@@ -347,7 +347,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
-
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.milahaina
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
