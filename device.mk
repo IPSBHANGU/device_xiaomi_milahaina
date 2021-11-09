dd1eab4207aa455b15213e0c33c87c167adb9fdf
IPSBHANGU <inderpreetbhangu2@gmail.com>
2021-11-09 16:59:13 +0100

    milahaina: Build base dummy & build required vendor modules
     * minjail_vendor & gui_vendor

--- device.mk
+++ device.mk
@@ -167,6 +167,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
 
@@ -221,11 +223,13 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libavservices_minijail \
     libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
     libmm-omxcore \
     libstagefright_softomx.vendor \
-    libstagefrighthw
+    libstagefrighthw \
+    libgui_vendor
 
 # NFC
 $(call inherit-product, vendor/nxp/opensource/commonsys/packages/apps/Nfc/nfc_system_product.mk)
