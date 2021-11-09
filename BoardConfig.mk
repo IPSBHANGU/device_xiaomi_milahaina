f9a6b9b8b5c5ce13f85e121322f20f4fe1dfa442
IPSBHANGU <inderpreetbhangu2@gmail.com>
2021-11-09 16:35:39 +0100

    milahaina: Enforce SELinux.
    Change-Id: Ifb1ff0b6aa6f62f7c432199f1ff8b14f2e2d7d8a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.selinux=permissive \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
     console=ttyMSM0,115200n8 \
@@ -163,6 +162,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # WLAN
 BOARD_HAS_QCOM_WLAN := true
