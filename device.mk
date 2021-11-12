bb3266c26c46572481dbd48d9769f4249d722ed0
IPSBHANGU <inderpreetbhangu2@gmail.com>
2021-11-12 19:28:08 +0100

    milahaina: Switch to Qti Health hal

--- device.mk
+++ device.mk
@@ -161,8 +161,11 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Health
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/healthd-ext
+
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
 # HIDL
